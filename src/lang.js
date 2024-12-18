480e302795942c9fc077fd6ead568fe06a60aaa6
Rodrigo <rodrigorodriguescosta@gmail.com>
2024-12-18 15:21:42 -0300

    Added support for ptBr and spanish

--- src/lang.js
+++ src/lang.js
@@ -13,5 +13,35 @@ export const lang = {
         pattern: "Please match the requested format",
         equals: "The two fields do not match",
         default: "Please enter a correct value"
+    },
+    ptBr: {
+        required: "Este campo é obrigatório",
+        email: "Este campo requer um endereço de e-mail válido",
+        number: "Este campo requer um número",
+        integer: "Este campo requer um valor inteiro",
+        url: "Este campo requer uma URL de site válida",
+        tel: "Este campo requer um número de telefone válido",
+        maxlength: "O comprimento deste campo deve ser < ${1}",
+        minlength: "O comprimento deste campo deve ser > ${1}",
+        min: "O valor mínimo para este campo é ${1}",
+        max: "O valor máximo para este campo é ${1}",
+        pattern: "Por favor, corresponda ao formato solicitado",
+        equals: "Os dois campos não correspondem",
+        default: "Por favor, insira um valor correto"
+    },
+    es: {
+        required: "Este campo es obligatorio",
+        email: "Este campo requiere una dirección de correo electrónico válida",
+        number: "Este campo requiere un número",
+        integer: "Este campo requiere un valor entero",
+        url: "Este campo requiere una URL de sitio web válida",
+        tel: "Este campo requiere un número de teléfono válido",
+        maxlength: "La longitud de este campo debe ser < ${1}",
+        minlength: "La longitud de este campo debe ser > ${1}",
+        min: "El valor mínimo para este campo es ${1}",
+        max: "El valor máximo para este campo es ${1}",
+        pattern: "Por favor, coincida con el formato solicitado",
+        equals: "Los dos campos no coinciden",
+        default: "Por favor, ingrese un valor correcto"
     }
 };
